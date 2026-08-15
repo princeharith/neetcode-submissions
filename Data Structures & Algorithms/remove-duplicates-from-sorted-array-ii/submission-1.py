e3c5f098b30643bb498b3904545bb455f0138229
Harry <96931168+princeharith@users.noreply.github.com>
2026-08-15 17:13:02 -0400

    Add: remove-duplicates-from-sorted-array-ii - submission-1

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array-ii/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        #[1,2,2,2]
+        #       l
+        #         r
+        
+        l, r = 0,0
+
+        while r < len(nums):
+            count = 1
+            while r + 1 < len(nums) and nums[r] == nums[r+1]:
+                count += 1
+                r += 1
+            
+            for _ in range(min(2, count)):
+                nums[l] = nums[r]
+                l += 1
+           
+            r += 1
+        return l
+                    
+        
+        
+        
+
+
+        
