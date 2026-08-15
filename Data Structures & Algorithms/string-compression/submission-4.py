96bfd83abcd42457c048cf88a2c411563e4f3dd1
Harry <96931168+princeharith@users.noreply.github.com>
2026-08-15 17:34:50 -0400

    Add: string-compression - submission-4

---
+++ Data Structures & Algorithms/string-compression/submission-4.py
@@ -0,0 +1,35 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        # ["a","a","a","a","a","b","c","c","c","c","c"]
+        #                       r
+        #                       l
+        #count = 5
+
+        l,w = 0,0
+
+        while l < len(chars):
+            r = l + 1
+            chars[w] = chars[l]
+            w += 1
+            while r < len(chars) and chars[l] == chars[r]:
+                r += 1
+            if (r-l) > 1:
+                for digit in str(r-l):
+                    chars[w] = digit
+                    l += 1
+                    w += 1
+            l = r
+        
+        return w
+        
+
+            
+
+
+
+        
+        
+                
+
+
+
