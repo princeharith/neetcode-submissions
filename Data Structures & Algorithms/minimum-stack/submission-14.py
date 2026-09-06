bfd4fbe05ff93d3912559beea64983bd336eb80a
Harry <96931168+princeharith@users.noreply.github.com>
2026-09-06 12:57:25 -0400

    Add: minimum-stack - submission-14

---
+++ Data Structures & Algorithms/minimum-stack/submission-14.py
@@ -0,0 +1,29 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.global_min = float('inf')
+
+        self.stack = [
+
+        ]
+        
+
+    def push(self, val: int) -> None:
+        self.global_min = min(self.global_min, val)
+        self.stack.append((val, self.global_min))
+
+    def pop(self) -> None:
+        #update min after popping
+        self.stack.pop()
+        if self.stack:
+            self.global_min = self.stack[-1][1]
+        else:
+            self.global_min = float('inf')
+        
+    def top(self) -> int:
+        return self.stack[-1][0]
+        
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+        
